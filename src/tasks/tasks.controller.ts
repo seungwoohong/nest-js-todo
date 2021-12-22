506195980b84ba0fadedf207ba9c252dc3380a66
seungwoohong <alwls601@naver.com>
2021-12-23 01:27:20 +0900

    feat(task): update and delete endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,15 @@
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksService } from './tasks.service';
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { TaskStatus } from './tasks.model';
 
 @Controller('/tasks')
 export class TasksController {
@@ -11,8 +20,23 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTask(@Param('id') id: string, @Body('status') status: TaskStatus) {
+    return this.tasksService.updateTask(id, status);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string) {
+    this.tasksService.deleteTask(id);
   }
 }
